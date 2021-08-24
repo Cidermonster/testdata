171fa297f9bb3a4966b7ce5ed20b33626cdd1f7c
Cidermonster <87560710+Cidermonster@users.noreply.github.com>
2021-08-24 15:18:26 +0900

    Update upload.js

--- upload.js
+++ upload.js
@@ -17,3 +17,10 @@ function previewFile(file) {
   reader.readAsDataURL(file);
 }
 
+const file = document.getElementById("example").files[0];
+// フォームデータを作成
+const formData = new FormData();
+// avatarというフィールド名でファイルを追加
+formData.append("avatar", file);
+// アップロード
+fetch(送信先のURL, { method: "POST", body: formData });
