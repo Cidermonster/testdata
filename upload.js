e53509337bdeaafd2d51870a971351bb9981a379
Cidermonster <87560710+Cidermonster@users.noreply.github.com>
2021-08-24 15:40:20 +0900

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,26 +1,16 @@
 function previewFile(file) {
-  // プレビュー画像を追加する要素
   const preview = document.getElementById('preview');
-
-  // FileReaderオブジェクトを作成
   const reader = new FileReader();
-
-  // ファイルが読み込まれたときに実行する
   reader.onload = function (e) {
-    const imageUrl = e.target.result; // 画像のURLはevent.target.resultで呼び出せる
-    const img = document.createElement("img"); // img要素を作成
-    img.src = imageUrl; // 画像のURLをimg要素にセット
-    preview.appendChild(img); // #previewの中に追加
+    const imageUrl = e.target.result;
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    preview.appendChild(img);
   }
 
-  // いざファイルを読み込む
   reader.readAsDataURL(file);
 }
-
 const file = document.getElementById("example").files[0];
-// フォームデータを作成
 const formData = new FormData();
-// avatarというフィールド名でファイルを追加
 formData.append("avatar", file);
-// アップロード
-fetch(送信先のURL, { method: "POST", body: formData });
+fetch(送信先URL, { method: "POST", body: formData });
